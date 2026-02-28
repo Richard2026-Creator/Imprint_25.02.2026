d86cb5a9011ad2f09ebf8296c8b2b17c2e5ac71b
Richard Leyens <richard@creativise.info>
2026-02-28 13:59:43 +0000

    Reduce card radius to 2xl

--- components/SwipeCard.tsx
+++ components/SwipeCard.tsx
@@ -8,10 +8,10 @@ interface SwipeCardProps {
 
 export const SwipeCard: React.FC<SwipeCardProps> = ({ image }) => {
   return (
-    <div className="relative w-full h-full bg-stone-100 rounded-[3.5rem] overflow-hidden ios-shadow border border-stone-100 select-none">
-      <img 
-        src={image.url} 
-        alt="Interior" 
+    <div className="relative w-full h-full bg-stone-100 rounded-2x1 overflow-hidden ios-shadow border border-stone-100 select-none">
+      <img
+        src={image.url}
+        alt="Interior"
         className="w-full h-full object-cover pointer-events-none"
       />
     </div>
