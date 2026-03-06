0cde434726c825debb43f18ae03b144af7f919c9
Richard Leyens <richard@creativise.info>
2026-03-06 08:09:45 +0000

    lower dragElastic setting to 0.15

--- views/Discovery.tsx
+++ views/Discovery.tsx
@@ -71,7 +71,7 @@ const DiscoveryCard: React.FC<DiscoveryCardProps> = ({ image, lastDirection, onS
         backfaceVisibility: 'hidden'
       }}
 
-      dragElastic={{ left: 0.25, right: 0.25 }}
+      dragElastic={{ left: 0.15, right: 0.15 }}
       dragMomentum={false}
       touchAction="pan-x"
       style={{ touchAction: 'pan-x', x: dragX, rotate, opacity }}
